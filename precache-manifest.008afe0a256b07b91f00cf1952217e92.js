ed6dfa7ca9cdcc0741411ebe3b9ed958b1c96ba3
Noel De Martin <noeldemartin@gmail.com>
2020-03-22 11:13:36 +0100

    deploy

---
+++ precache-manifest.008afe0a256b07b91f00cf1952217e92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bd15e1b1e3857dbecd",
+    "url": "/tailwindcss-colors-generator/css/app.86372d17.css"
+  },
+  {
+    "revision": "7754e2fe1dd77c6e2579df3e31366cda",
+    "url": "/tailwindcss-colors-generator/index.html"
+  },
+  {
+    "revision": "67bd15e1b1e3857dbecd",
+    "url": "/tailwindcss-colors-generator/js/app.24b35555.js"
+  },
+  {
+    "revision": "639075d2664fd5fcb56d",
+    "url": "/tailwindcss-colors-generator/js/chunk-vendors.fdff9cf8.js"
+  },
+  {
+    "revision": "3dcd8bed6bf3f6c4fab758ad05e84ab4",
+    "url": "/tailwindcss-colors-generator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tailwindcss-colors-generator/robots.txt"
+  }
+]);
